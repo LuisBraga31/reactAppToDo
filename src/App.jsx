f22c4e3fdf497844c83b6c563501bac96acd4f71
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-08-22 22:44:48 -0300

    Creating function to add tasks to html

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,19 @@ function App() {
     }
   ]);
 
+  const addTodo = (text, category) => {
+
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    }];
+
+    setTodos(newTodos);
+
+  }
+
   return (
     <div className="app"> 
     
@@ -41,7 +54,7 @@ function App() {
 
       </div>
 
-      <TodoForm/>
+      <TodoForm addTodo={addTodo}/>
     
     </div>
   )
