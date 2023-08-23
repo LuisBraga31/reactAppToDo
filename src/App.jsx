6f8fc07a6e8d04aaa636897d0ec4f630014b01c1
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-08-22 22:40:58 -0300

    Add states to TodoForm and form settings

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       <div className="todo-list">
         {todos.map((todo) => (
           
-          <Todo todo={todo} />
+          <Todo key={todo.id} todo={todo} />
         ))}
 
       </div>
